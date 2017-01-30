39eee4bd7b2375f013862040e6aa603e02fc81b4
Broyojo <dha@xoba.com>
2017-01-30 14:38:20 -0500

    Create Canvas.js

---
+++ Canvas.js
@@ -0,0 +1,49 @@
+var global = require('./global');
+
+class Canvas {
+    constructor(params) {
+        this.directionLock = false;
+        this.target = global.target;
+        this.reenviar = true;
+        this.socket = global.socket;
+        this.directions = [];
+        var self = this;
+
+        this.cv = document.getElementById('cvs');
+        this.cv.width = global.screenWidth;
+        this.cv.height = global.screenHeight;
+        this.cv.addEventListener('mousemove', this.gameInput, false);
+        this.cv.addEventListener('mouseout', this.outOfBounds, false);
+        this.cv.addEventListener('keypress', this.keyInput, false);
+        this.cv.addEventListener('keyup', function(event) {
+            self.reenviar = true;
+            self.directionUp(event);
+        }, false);
+        this.cv.addEventListener('keydown', this.directionDown, false);
+        this.cv.addEventListener('touchstart', this.touchInput, false);
+        this.cv.addEventListener('touchmove', this.touchInput, false);
+        this.cv.parent = self;
+        global.canvas = this;
+    }
+
+    // Function called when a key is pressed, will change direction if arrow key.
+    directionDown(event) {
+    	var key = event.which || event.keyCode;
+        var self = this.parent; // have to do this so we are not using the cv object
+    	if (self.directional(key)) {
+    		self.directionLock = true;
+    		if (self.newDirection(key, self.directions, true)) {
+    			self.updateTarget(self.directions);
+    			self.socket.emit('0', self.target);
+    		}
+    	}
+    }
+
+    // Function called when a key is lifted, will change direction if arrow key.
+    directionUp(event) {
+    	var key = event.which || event.keyCode;
+    	if (this.directional(key)) { // this == the actual class
+    		if (this.newDirection(key, this.directions, false)) {
+    			this.updateTarget(this.directions);
+    			if (this.directions.length === 0) this.directionLock = false;
+    			this.socket.emit('0', this.target);
