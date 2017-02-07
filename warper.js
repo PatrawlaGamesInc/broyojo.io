0488e6f7b92475126e411aec17487a9398974aa4
FlamingPhoenix2YT <flamingphoenix2004@gmail.com>
2017-02-06 21:28:02 -0500

    Create warper.js

---
+++ warper.js
@@ -0,0 +1,17 @@
+noStroke();
+rotate(45);
+fill(0, 255, 26);
+rect(300,1,100,100);
+//
+fill(0, 0, 0);
+ellipse(350, 50, 100, 50);
+//
+noFill();
+stroke(5, 5, 5);
+strokeWeight(5);
+ellipse(350, 50, 100, 100);
+fill(255, 0, 255);
+//pink circles should orbit with finished code//
+noStroke();
+ellipse(230, 60, 50, 50);
+ellipse(466, 53, 50, 50);
